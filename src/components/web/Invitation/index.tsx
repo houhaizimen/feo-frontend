9d768af99b469747bcf0b3fa65300abdae6dbb96
kilig <1679997020@qq.com>
2022-12-11 23:49:04 +0800

    星星

--- src/components/web/Invitation/index.tsx
+++ src/components/web/Invitation/index.tsx
@@ -11,7 +11,13 @@ const Index = () => {
     {
       list.map(item => <p key={item}>{item}</p>)
     }
-    <img src="assets/invitation.png" alt="" />
+    <div className='web-home-invitation-star'>
+      <img className='invitation' src="assets/invitation.png" alt="" />
+      <img className='bg' src="assets/invitation-bg.png" alt="" />
+      <img className='star-1' src="assets/star-1.png" alt="" />
+      <img className='star-2' src="assets/star-2.png" alt="" />
+      <img className='star-3' src="assets/start-3.png" alt="" />
+    </div>
   </div>
 }
 
