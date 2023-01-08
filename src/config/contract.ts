2ea49116cf3ad40e2c353727c295d8d7c5673650
kilig <1679997020@qq.com>
2023-01-08 20:43:32 +0800

    修改合约地址

--- src/config/contract.ts
+++ src/config/contract.ts
@@ -3,6 +3,6 @@ import { Address } from './types'
 export const contracts: Record<string, Address> = {
   Runners: {
     1: '0x6a3d1E97A6a0976f8Fc838370781F278cb7b339F',
-    5: '0xDb16dB13d8Cd36cc2eEE5Abc83dedB458B5BB822'
+    5: '0x0af00Af058c82D2FE2b49eeAe4920a3Df0BF4EeF'
   }
 }
