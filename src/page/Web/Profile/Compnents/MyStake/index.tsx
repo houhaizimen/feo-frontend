004f77435dc2eb1562794c8b7793f9084662867f
chengfang <1679997020@qq.com>
2023-02-10 23:38:50 +0800

    fix bug

--- src/page/Web/Profile/Compnents/MyStake/index.tsx
+++ src/page/Web/Profile/Compnents/MyStake/index.tsx
@@ -3,11 +3,16 @@ import ContainerBg from '@/components/common/ContainerBg'
 import Button from '@/components/common/Button'
 
 const Index = () => {
+  const TITLE_LIST = ['NFT', 'Start date', 'End date', 'Expected fragments']
+  const RES_LIST = []
   return <div className='web-profile-my-stake'>
     <h1 className='profile-title'>My staking</h1>
     <ContainerBg className='web-profile-my-stake-wrap'>
-      {
+      {/* {
         <p>You can earn more candies and fragments of treasure map by staking.</p>
+      } */}
+      {
+
       }
       <Button size='medium'>To staking</Button>
     </ContainerBg>
