74b7f954a8ab97fb93fd36fe3dd89f4e94deed29
chengfang <1679997020@qq.com>
2023-02-10 14:24:05 +0800

    item

--- src/page/Web/Stake/Components/Step2/index.tsx
+++ src/page/Web/Stake/Components/Step2/index.tsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import { useWeb3React } from '@web3-react/core'
 import ContainerBg from '@/components/common/ContainerBg'
-import Button from '@/components/common/Button'
 
 const Index = () => {
   const { account } = useWeb3React()
-  return <div className='web-stake-step1'>
-    <h1 className='web-stake-step1-title'>STEP<span>2</span>SELECT NFT TO BE STAKED</h1>
-    <ContainerBg className='web-stake-step1-cont'>
-    </ContainerBg>
-  </div>
+  return <>
+    {
+      !account && <div className='web-stake-step1 web-stake-step2'>
+        <h1 className='web-stake-step1-title'>STEP<span>2</span>SELECT NFT TO BE STAKED</h1>
+        <ContainerBg className='web-stake-step2-cont'>
+          <div className='web-stake-step2-cont-item'>
+            <h1>YOUR COLLECTION</h1>
+          </div>
+        </ContainerBg>
+      </div>
+    }
+  </>
 }
 
 export default Index
