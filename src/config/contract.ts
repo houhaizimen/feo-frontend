3451ba2378b1fbca2336a6eabce6a97fde604c20
chengfang <1679997020@qq.com>
2023-02-17 12:31:17 +0800

    初始化合约

--- src/config/contract.ts
+++ src/config/contract.ts
@@ -4,5 +4,13 @@ export const contracts: Record<string, Address> = {
   Runners: {
     1: '0x6a3d1E97A6a0976f8Fc838370781F278cb7b339F',
     5: '0x55Db7541Dd1C114895a81F3cfE547455cC916961'
+  },
+  MAINFT: {
+    1: '0x6a3d1E97A6a0976f8Fc838370781F278cb7b339F',
+    5: '0x792099c2d8a9321555A3423896bBffFB7f3c8F1e'
+  },
+  KachousenNFT: {
+    1: '0x6a3d1E97A6a0976f8Fc838370781F278cb7b339F',
+    5: '0x388019c25D974fA405a67514C3079423187f2d82'
   }
 }
--- src/config/getContract.ts
+++ src/config/getContract.ts
@@ -2,6 +2,8 @@ import { ethers, ContractInterface } from 'ethers'
 import { Address } from './types'
 import { JSONRPCProvider, getChainId } from '@/utils/provider'
 import RunnersABI from '@/abi/Runners.json'
+import kachousenABI from '@/abi/kachousen.json'
+import MAIABI from '@/abi/MAI.json'
 import { contracts } from './contract'
 
 export const getContract = (abi: ContractInterface, address: Address, signer?: ethers.Signer | ethers.providers.Provider) => {
@@ -12,4 +14,12 @@ export const getContract = (abi: ContractInterface, address: Address, signer?: e
 
 export const getRunnersContract = (signer?: ethers.Signer | ethers.providers.Provider): any => {
   return getContract(RunnersABI, contracts.Runners, signer)
+}
+
+export const getMAIContract = (signer?: ethers.Signer | ethers.providers.Provider): any => {
+  return getContract(MAIABI, contracts.MAINFT, signer)
+}
+
+export const getkachousenContract = (signer?: ethers.Signer | ethers.providers.Provider): any => {
+  return getContract(kachousenABI, contracts.KachousenNFT, signer)
 }
