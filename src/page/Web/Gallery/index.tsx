14d10cb72a9a71ebaff1a800feb115507c35644e
chengfang <1679997020@qq.com>
2023-02-17 19:05:28 +0800

    新增ui

--- src/page/Web/Gallery/index.tsx
+++ src/page/Web/Gallery/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
+import { useNavigate } from 'react-router-dom'
 import Footer from '@/components/web/Footer'
 
 const Index = () => {
   const { t } = useTranslation()
   const ts: Record<string, any> = t('GALLERY', { returnObjects: true })
+  const navigate = useNavigate()
+  const handleJump = (link: string) => {
+    if (!link) return
+    navigate(link)
+  }
   return <div className='web-gallery'>
     <div className='web-gallery-cont'>
       <h1 className='title' dangerouslySetInnerHTML={{ __html: t('GALLERY.title', { symbol: '<br />' }) }} />
@@ -23,7 +29,7 @@ const Index = () => {
       <ul className='card'>
         {
           ts.GALLERY_BOTTOM_LIST.map((item: any) => {
-          return <li key={item.title} style={{
+          return <li key={item.title} onClick={() => handleJump(item.link)} style={{
             backgroundImage: `url(assets/${item.icon}.png)`
           }}>
             <h1>{item.title}</h1>
