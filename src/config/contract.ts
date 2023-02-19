09dcfa9e92449a55c1bc549f477ae6fffae06c27
chengfang <1679997020@qq.com>
2023-02-19 22:31:04 +0800

    更新mint contract address

--- src/config/contract.ts
+++ src/config/contract.ts
@@ -3,7 +3,7 @@ import { Address } from './types'
 export const contracts: Record<string, Address> = {
   Runners: {
     1: '0x6a3d1E97A6a0976f8Fc838370781F278cb7b339F',
-    5: '0x58191BDB2711010bBC94d9bfe5b364D0852F8610'
+    5: '0x5d730569F257Ac42f97C378C9A4E5E14C16A79e6'
   },
   KachousenNFT: {
     1: '0x6a3d1E97A6a0976f8Fc838370781F278cb7b339F',
