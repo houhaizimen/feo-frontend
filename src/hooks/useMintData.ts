12899e83c1b976fac49f70610dc2af91018ece61
chengfang <1679997020@qq.com>
2023-02-19 21:28:34 +0800

    fix bug

--- src/hooks/useMintData.ts
+++ src/hooks/useMintData.ts
@@ -1,11 +1,12 @@
 import { useCallback, useEffect, useMemo, useState } from 'react'
 import Runners from '@/class/Runners'
 export const useMintData = (account: string, balance: string) => {
-  const { getBlanceOf, getwlMintStartTime, getwlMintEndTime, getpMintStartTime, getpMintEndTime, getMaxMinted } = Runners
-  const [count, setCount] = useState<number>(0)
+  // const { getBlanceOf, getwlMintStartTime, getwlMintEndTime, getpMintStartTime, getpMintEndTime, getMaxMinted } = Runners
+  const { getwlMintStartTime, getwlMintEndTime, getpMintStartTime, getpMintEndTime } = Runners
+  // const [count, setCount] = useState<number>(0)
   const [isWhiteTime, setIsWhiteTime] = useState<boolean>(false)
   const [isPTime, setIsPTime] = useState<boolean>(false)
-  const [maxCount, setMaxCount] = useState<number>(0)
+  // const [maxCount, setMaxCount] = useState<number>(0)
   const handleGetStartTime = useCallback(async () => {
     const wStart = await getwlMintStartTime()
     const wEnd = await getwlMintEndTime()
@@ -22,41 +23,41 @@ export const useMintData = (account: string, balance: string) => {
     }
   }, [getwlMintStartTime, getwlMintEndTime, getpMintStartTime, getpMintEndTime])
 
-  const handleBalanceOf = useCallback(async (account: string) => {
-    const res = await getBlanceOf(account)
-    setCount(res)
-  }, [getBlanceOf])
+  // const handleBalanceOf = useCallback(async (account: string) => {
+  //   const res = await getBlanceOf(account)
+  //   setCount(res)
+  // }, [getBlanceOf])
 
-  const handleMaxMinted = useCallback(async () => {
-    const res = await getMaxMinted()
-    setMaxCount(res)
-  }, [getMaxMinted])
+  // const handleMaxMinted = useCallback(async () => {
+  //   const res = await getMaxMinted()
+  //   setMaxCount(res)
+  // }, [getMaxMinted])
 
-  useEffect(() => {
-    void handleMaxMinted()
-   }, [handleMaxMinted])
+  // useEffect(() => {
+  //   void handleMaxMinted()
+  //  }, [handleMaxMinted])
 
   useEffect(() => {
     void handleGetStartTime()
   }, [handleGetStartTime])
 
-  useEffect(() => {
-    if (account) void handleBalanceOf(account)
-  }, [handleBalanceOf, account])
-  const max = useMemo(() => {
-    if (isWhiteTime) {
-      return maxCount - count
-    }
-    return 100
-  }, [isWhiteTime, count, maxCount])
+  // useEffect(() => {
+  //   if (account) void handleBalanceOf(account)
+  // }, [handleBalanceOf, account])
+  // const max = useMemo(() => {
+  //   if (isWhiteTime) {
+  //     return maxCount - count
+  //   }
+  //   return 100
+  // }, [isWhiteTime, count, maxCount])
   const disabled = useMemo(() => {
     return (!isWhiteTime && !isPTime) || !account || !balance
   }, [isWhiteTime, isPTime, account, balance])
   return {
-    max,
+    // max,
     disabled,
-    handleGetStartTime,
-    handleBalanceOf,
-    maxCount
+    handleGetStartTime
+    // handleBalanceOf,
+    // maxCount
   }
 }
