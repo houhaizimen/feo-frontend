c9d82ace1a250ce878eb683453f5637109f64879
chengfang <1679997020@qq.com>
2023-02-21 17:38:47 +0800

    换

--- src/config/contract.ts
+++ src/config/contract.ts
@@ -3,7 +3,7 @@ import { Address } from './types'
 export const contracts: Record<string, Address> = {
   Runners: {
     1: '0x6a3d1E97A6a0976f8Fc838370781F278cb7b339F',
-    5: '0x5d730569F257Ac42f97C378C9A4E5E14C16A79e6'
+    5: '0x65c177573B68a5c0FF9f9d73230D5b39BB80e939'
   },
   KachousenNFT: {
     1: '0x6a3d1E97A6a0976f8Fc838370781F278cb7b339F',
