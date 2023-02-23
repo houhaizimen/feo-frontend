7dfa257e484e0745d7219ef06c85fa58920dbca6
chengfang <1679997020@qq.com>
2023-02-23 16:34:44 +0800

    fix bug

--- src/hooks/useMintData.ts
+++ src/hooks/useMintData.ts
@@ -76,7 +76,6 @@ export const useMintData = (account: string, balance: string) => {
   // const disabled = useMemo(() => {
   //   return (!isWhiteTime && !isPTime) || !balance
   // }, [isWhiteTime, isPTime, balance])
- 
   return {
     // max,
     pPrice,
