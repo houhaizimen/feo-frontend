f61f8bfef3c54a7e7a8b4fee981f78ca1f1243e4
chengfang <1679997020@qq.com>
2023-03-14 15:45:26 +0800

    初始化扇子

--- src/class/kachousen.ts
+++ src/class/kachousen.ts
@@ -1,9 +1,14 @@
-// import { getkachousenContract } from '@/config/getContract'
+import { getkachousenContract } from '@/config/getContract'
 // import { Web3Provider } from '@ethersproject/providers'
-// import { getBalanceAmount, getAountToBigHex } from '@/utils/formatAmount'
+import { getBalanceAmount } from '@/utils/formatAmount'
 // import estimateGas from '@/utils/estimateGas'
 // import { getMerkleTree } from '@/utils/merkletree'
 
 class Kachousen {
+  getBalanceOf = async (account: string) => {
+    const contract = getkachousenContract()
+    const res = await contract.balanceOf(account)
+    return getBalanceAmount(res?._hex ?? 0, 0)
+  }
 }
 export default new Kachousen()
