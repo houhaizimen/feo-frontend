77e07745d05415fad205e08a6a5153c958e722b6
kilig <kilig@cashrocket.com.ph>
2023-09-14 17:01:34 +0800

    fix assign

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -4,19 +4,24 @@ import { getRegAndLogin, getUserBagIndex } from '@/api'
 
 const useLogin = () => {
   const { account, library } = useWeb3React()
+  const handlAsign = useCallback(async () => {
+    if (account) {
+      const signer = library.getSigner(account)
+      const message = new Date().getTime().toString()
+      const signature = await signer.signMessage(message)
+      const res = await getRegAndLogin({ address: account, message, signature })
+      localStorage.setItem('token', res?.data?.token)
+    }
+  }, [library, account])
   const ServerLogin = useCallback(async () => {
     const token = localStorage.getItem('token')
-    const res = await getUserBagIndex()
-    if (res.code === '503' || !token) {
-      if (account) {
-        const signer = library.getSigner(account)
-        const message = new Date().getTime().toString()
-        const signature = await signer.signMessage(message)
-        const res = await getRegAndLogin({ address: account, message, signature })
-        localStorage.setItem('token', res?.data?.token)
-      }
+    if (!token) {
+      handlAsign()
+      return
     }
-  }, [account, library])
+    const res = await getUserBagIndex()
+    if (res.code === '503') handlAsign()
+  }, [handlAsign])
   return {
     ServerLogin
   }
